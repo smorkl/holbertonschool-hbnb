11df57d2ea5df4fe8227407a4d1b73e2ef21dc3f
smorkl <smsalazar0319@gmail.com>
2024-11-14 18:35:45 -0500

    create

--- part2/HBnB/app/__init__.py
+++ part2/HBnB/app/__init__.py
@@ -53,7 +53,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.reviews import api as reviews_ns
     from app.api.v1.auth import api as auth_ns
 
-    # Register the users namespace
+    # Register the users namespace, url base
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
